2ac03e263ea11e5673e214acc56e853c0b60c36e
MR x BUNTY <160619973+MRxBunty@users.noreply.github.com>
2024-05-29 00:25:13 -0600

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,56 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+
+const anwers: {
+  numberone: number
+  numbertwo: number
+  operator: string
+} =await inquirer.prompt([
+  { type: "number",
+  name: "numberOne",
+  message: "enter your first number"
+},
+{ type: "number",
+  name: "numbertwo",
+  message: "enter your second number"
+},
+{ type: "list",
+name: "operator",
+message: "enter your operator",
+choices: ["+", "-", "*", "/"]}  
+
+])
+
+const {numberone, numbertwo, operator } = anwers
+
+let result
+switch (operator) {
+  case "+":
+    result = numberone+numbertwo;
+    break;
+
+    case "-":
+      result = numberone-numbertwo;
+      break;
+
+      case "*":
+    result = numberone*numbertwo;
+    break;
+
+    case "/":
+    result = numberone/numbertwo;
+    break;
+    default:
+      console.log("invalid operator");
+}
+console.log(`${numberone} ${operator} ${numbertwo} = ${result}`);
+
+
+
+
+
+
+
+
+
